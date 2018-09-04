9b4e86d9723acea3d41afafc1dac55e8a986879d
Florian Neveu <hello@fnev.eu>
2018-09-05 00:42:00 +0200

    Add help command and ready event

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 
 exports.run = (client, message, args) => {
-  if (message.author.id !== client.config.ownerID) return;
+    if (message.author.id !== client.config.ownerID) return;
 
-  let newPrefix = args.shift();
+    let newPrefix = args.shift();
 
-  if (undefined === newPrefix) return;
+    if (undefined === newPrefix) return;
 
-  client.config.prefix = newPrefix;
-  fs.writeFile("./config.json", JSON.stringify(client.config), (err) => console.error);
-  message.channel.send("Le nouveau préfixe est `" + client.config.prefix + "`.");
+    client.config.prefix = newPrefix;
+    fs.writeFile("./config.json", JSON.stringify(client.config), (err) => console.error);
+    message.channel.send("Le nouveau préfixe est `" + client.config.prefix + "`.");
 };
 
 exports.conf = {
-  enabled: true,
-  aliases: [],
-  permLevel: "User"
+    enabled: true,
+    aliases: [],
+    permLevel: "User"
 };
 
 exports.help = {
-  name: "prefix",
-  category: "Miscelaneous",
-  description: "Permet le changement du prefix du bot.",
-  usage: "prefix <prefix>"
+    name: "prefix",
+    category: "Miscelaneous",
+    description: "Permet le changement du prefix du bot.",
+    usage: "prefix <prefix>"
 };
